af89ea9c75aeb92ce5754891945146ef7c13ae1f
cryxy <cryxy@gmx.de>
2021-02-20 22:07:41 +0100

    refactor logging

--- src/main/resources/start_debug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-prog_arg="-Dlogback.configurationFile=conf/logback_debug.xml"
-prog_arg="${prog_arg} -Djava.library.path=/usr/lib/jni"
-prog_arg="${prog_arg} -Dgnu.io.rxtx.SerialPorts=/dev/ttyAMA0"
-
-
-java $prog_arg -jar lib/optolink-jar-with-dependencies.jar
